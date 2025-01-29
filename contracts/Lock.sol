be794de94328e33502ab99ae9e936be19db9160e
ehsan shariati <ehsan6sha@gmail.com>
2025-01-29 13:19:16 -0500

    [L-01] The constructor was still payable
    Fixed by removing the payable from constructor

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,16 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
 contract Lock {
     uint public unlockTime;
     address payable public owner;
 
     event Withdrawal(uint amount, uint when);
 
-    constructor(uint _unlockTime) payable {
+    constructor(uint _unlockTime) {
         require(
             block.timestamp < _unlockTime,
             "Unlock time should be in the future"
