844cf1d43dc1d3cf566ee1424f51e48162f5e354
ehsan shariati <ehsan6sha@gmail.com>
2025-02-11 20:09:36 -0500

    Update deployLocalTestnetMining.ts

--- scripts/deployLocalTestnetMining.ts
+++ scripts/deployLocalTestnetMining.ts
@@ -175,7 +175,7 @@ async function main() {
     await rewardsContract.connect(ownerWallet).initiateTGE();
 
     // Add wallet mapping
-    const testWallet = "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199";
+    const testWallet = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
     const substrateAddr = "5GWck2Qtq9MzsUhLpx2ksLrd3zZ3tBRAGDRanArR2AfYfAxH";
     await rewardsContract.connect(ownerWallet).batchAddAddresses(
         [testWallet],
