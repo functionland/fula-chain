3893b0db04fed0758da9d4d2fdc8b254fccda01e
ehsan shariati <ehsan6sha@gmail.com>
2025-12-23 22:41:32 -0500

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -123,6 +123,75 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: "JGD6ENM6P2G5XUS4VSCJJYGXR3RXCG2TEN",
+    /* //Activate for skale and iotex verify  and deactivate for base and ethereum verification
+    apiKey: {
+      mainnet: vars.has("ETHERSCAN_API_KEY")
+        ? vars.get("ETHERSCAN_API_KEY")
+        : "",
+      sepolia: vars.has("ETHERSCAN_API_KEY")
+        ? vars.get("ETHERSCAN_API_KEY")
+        : "",
+      base: vars.has("BASESCAN_API_KEY") ? vars.get("BASESCAN_API_KEY") : "",
+      "base-sepolia": vars.has("BASESCAN_API_KEY")
+        ? vars.get("BASESCAN_API_KEY")
+        : "",
+      "iotex-mainnet": vars.has("IOTEXSCAN_API_KEY")
+        ? vars.get("IOTEXSCAN_API_KEY")
+        : "arbitrary",
+      skale: vars.has("SKALESCAN_API_KEY")
+        ? vars.get("SKALESCAN_API_KEY")
+        : "arbitrary",
+      "iotex-testnet": vars.has("IOTEXSCAN_API_KEY")
+        ? vars.get("IOTEXSCAN_API_KEY")
+        : "arbitrary",
+      "skale-testnet": vars.has("SKALESCAN_API_KEY")
+        ? vars.get("SKALESCAN_API_KEY")
+        : "arbitrary",
+    },
+    customChains: [
+      {
+        network: "base",
+        chainId: 8453,
+        urls: {
+          apiURL: "https://api.basescan.org/api",
+          browserURL: "https://basescan.org",
+        },
+      },
+      {
+        network: "base-sepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://api-sepolia.basescan.org/api",
+          browserURL: "https://sepolia.basescan.org",
+        },
+      },
+      {
+        network: "iotex-mainnet",
+        chainId: 4689,
+        urls: {
+          apiURL: "https://iotexscout.io/api",
+          browserURL: "https://iotexscan.io",
+        },
+      },
+      {
+        network: "skale",
+        chainId: 2046399126,
+        urls: {
+          apiURL: "https://elated-tan-skat.explorer.mainnet.skalenodes.com/api",
+          browserURL: "https://elated-tan-skat.explorer.mainnet.skalenodes.com",
+        },
+      },
+      {
+        network: "skale-testnet",
+        chainId: 1444673419,
+        urls: {
+          apiURL:
+            "https://juicy-low-small-testnet.explorer.testnet.skalenodes.com/api",
+          browserURL: "https://europa-explorer.testnet.skalenodes.com",
+        },
+      },
+    ],
+    */
   },
   mocha: {
     timeout: 40000,
